ab3e578e592a5f613eb92f74cf3024d25ca0123c
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-10-08 22:50:17 +0530

    commit

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,9 +1,19 @@
-var updateBtns = document,getElementByClassName('update-cart')
+var updateBtns = document.getElementsByClassName('update-cart')
 
+for(var i = 0; i < updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', funtion(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId: ', productId, 'action:' , action)
 
-
-for(var i = 0; i < updateBtns.length; ){
-    
+        console.log('USER:',user)
+        if (user == 'AnonymousUser'){
+            console.log('User not Authenticated')
+        }
+        else{
+            console.log('User is Authenticated. Sending Data ...')
+        }
+    })
 }
 
 
